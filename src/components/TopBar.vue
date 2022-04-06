7c04dfa6f801c1a0ed12b7b404e1f21e5ed753cf
Gid <giddevanderven@gmail.com>
2022-04-06 10:59:13 +0200

    add theme styling and fonts

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,13 +1,21 @@
 <template>
-  <v-app-bar>
+  <v-app-bar
+  image="https://www.home.saxo/-/media/images/frontpage/announcement-ribbon.jpg?la=en&amp;h=153&amp;w=768&amp;mw=768&amp;hash=434EC65C5004F64E248EDE1D013875AD">
+     <v-img
+    class="mx-2"
+    src="https://www.home.saxo/-/media/global/logos/saxobank/saxobank.svg?h=1&w=1&la=en&hash=B7EF4C6FC7C9823028101C89D2431798"
+    max-height="50"
+    max-width="50"
+    contain
+  ></v-img>
     <v-app-bar-title> PreddyFraderTro </v-app-bar-title>
          <v-text-field v-model="accessToken"
             density="compact"
-            label="24 Hour Token"
+            label="24-Hour Token"
             variant="outlined"
           ></v-text-field>
-    <v-btn color="primary">Status</v-btn>
-    <v-btn color="secondary">Login</v-btn>
+    <!-- <v-btn color="primary">Status</v-btn> -->
+    <v-btn color="light" variant="contained-text">Login</v-btn>
   </v-app-bar>
 </template>
 
@@ -15,7 +23,7 @@
 export default {
   name: "TopBar",
   data: () => ({
-    accessToken: "foo"
+    accessToken: "Paste token here..."
   }),
 };
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,7 +5,28 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-  { theme: { defaultTheme: "dark" }, }
-)
+const Theme = {
+  dark: true,
+  colors: {
+    background: '#031329',
+    surface: '#031329',
+    primary: '#52D887',
+    'primary-darken-1': '#2AB762',
+    secondary: '#8F8AFF',
+    'secondary-darken-1': '#6661D4',
+    light: "#FFFFFF",
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  }
+}
+
+export default createVuetify({
+  theme: {
+    defaultTheme: 'Theme',
+    themes: {
+      Theme,
+    }
+  }
+})
