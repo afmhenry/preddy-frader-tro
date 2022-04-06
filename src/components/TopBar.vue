06d13dfdd7d38b165de19472380c74adee233dd8
Gid <giddevanderven@gmail.com>
2022-04-06 11:08:45 +0200

    styling

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar
-  image="https://www.home.saxo/-/media/images/frontpage/announcement-ribbon.jpg?la=en&amp;h=153&amp;w=768&amp;mw=768&amp;hash=434EC65C5004F64E248EDE1D013875AD">
+  image="/topbar-background.svg">
      <v-img
     class="mx-2"
     src="https://www.home.saxo/-/media/global/logos/saxobank/saxobank.svg?h=1&w=1&la=en&hash=B7EF4C6FC7C9823028101C89D2431798"
@@ -8,7 +8,7 @@
     max-width="50"
     contain
   ></v-img>
-    <v-app-bar-title> PreddyFraderTro </v-app-bar-title>
+    <v-app-bar-title>PreddyFraderTro</v-app-bar-title>
          <v-text-field v-model="accessToken"
             density="compact"
             label="24-Hour Token"
