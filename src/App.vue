3797a3ca6d1e5f0c6d1fb40c949e5356bc313a26
Frederik Fenger <frederikfenger@gmail.com>
2022-04-06 12:01:31 +0200

    Basic grid in place

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <v-app>
+  <v-app class="fill-height">
     <TopBar></TopBar>
 
-    <v-main>
-      <v-container> </v-container>
+    <v-main class="fill-height">
+      <v-container fluid class="d-flex flex-column fill-height">
+        <v-row style="height: 60%; max-height: 60%; min-height: 60%">
+          <v-col style="width: 20%; max-width: 20%">SEARCH</v-col>
+          <v-col>PI</v-col>
+          <v-col style="width: 20%; max-width: 20%">Trade</v-col>
+        </v-row>
+        <v-row style="">
+          <v-col class="d-flex flex-column">
+            <v-row>
+              <v-col>ORDERS</v-col>
+            </v-row>
+            <v-row>
+              <v-col>Positions</v-col>
+            </v-row>
+            <v-row style="height: 40px; max-height: 40px; min-height: 40px">
+              <v-col>Account stuff</v-col>
+            </v-row>
+          </v-col>
+          <v-col style="width: 30%; max-width: 30%">Events</v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
