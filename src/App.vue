9968b4a1e56ec2331338f9a763168205018e4b6d
ahen <10469240+afmhenry@users.noreply.github.com>
2022-04-06 13:09:16 +0200

    axios yo

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <v-main class="fill-height">
       <v-container fluid class="d-flex flex-column fill-height">
         <v-row style="height: 60%; max-height: 60%; min-height: 60%">
-          <v-col style="width: 20%; max-width: 20%">SEARCH</v-col>
+          <v-col style="width: 20%; max-width: 20%"><InstrumentModule /></v-col>
           <v-col>PI</v-col>
           <v-col style="width: 20%; max-width: 20%">Trade</v-col>
         </v-row>
@@ -29,13 +29,15 @@
 </template>
 
 <script>
-import TopBar from "./components/TopBar.vue";
 
+import InstrumentModule from "./components/InstrumentModule.vue";
+import TopBar from "./components/TopBar.vue";
 export default {
   name: "App",
 
   components: {
     TopBar,
+    InstrumentModule
   },
 
   data: () => ({}),
---
+++ src/components/InstrumentModule.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card>
+    <h1>Instruments</h1>
+    <v-text-field
+      class="mx-2"
+      v-model="keyword"
+      density="compact"
+      label="Instrument"
+      variant="outlined"
+      placeholder="Search..."
+    >
+    </v-text-field>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "InstrumentModule",
+  data: () => ({
+    keyword: "",
+  }),
+  watch: {
+    keyword: function (value) {
+      console.log(value);
+      
+    },
+  },
+};
+</script>
