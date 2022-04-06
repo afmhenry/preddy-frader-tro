6f2f394e453598fcab4129f7f6a486e15f2b3ded
Frederik Fenger <frederikfenger@gmail.com>
2022-04-06 11:24:07 +0200

    SHIT DOESN*T WORK

---
+++ src/components/LoginButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-text-field v-model="accessToken"
+            density="compact"
+            label="24-Hour Token"
+            variant="outlined"
+          ></v-text-field>
+    <!-- <v-btn color="light" variant="contained-text">Login</v-btn> -->
+</template>
+
+<script>
+export default {
+  name: "LoginButton",
+  data: () => ({
+    accessToken: "Paste token here..."
+  }),
+  watch: {
+    accessToken: function (value) {
+      localStorage.setItem('accessToken', value)
+    }
+  }
+};
+</script>
--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -9,21 +9,22 @@
     contain
   ></v-img>
     <v-app-bar-title class="font-weight-bold">PreddyFraderTro</v-app-bar-title>
-         <v-text-field v-model="accessToken"
-            density="compact"
-            label="24-Hour Token"
-            variant="outlined"
-          ></v-text-field>
+         <LoginButton />
     <!-- <v-btn color="primary">Status</v-btn> -->
     <v-btn color="light" variant="contained-text">Login</v-btn>
   </v-app-bar>
 </template>
 
 <script>
+import LoginButton from "./LoginButton.vue";
+
 export default {
   name: "TopBar",
   data: () => ({
     accessToken: "Paste token here..."
   }),
+  components: {
+    LoginButton
+  }
 };
 </script>
