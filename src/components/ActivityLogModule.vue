b647adfc8915a1bc34f893dfba152d006b94e1d6
ahen <10469240+afmhenry@users.noreply.github.com>
2022-04-07 13:34:35 +0200

    starting dev mode

---
+++ src/components/ActivityLogModule.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-card height="100%">
+    <v-icon
+      v-if="devMode"
+      style="float: right"
+      class="ma-1"
+      size="small"
+      color="secondary"
+      >mdi-information-outline</v-icon
+    >
+    <v-card-header>
+      <div>
+        <div class="font-weight-bold text-h6">Activity Log</div>
+        <br />
+
+        <div v-if="!subscriptionActive">
+          <div
+            class="text-overline text-error font-weight-bold"
+            style="font-size: 0.7rem !important; line-height: 0.8rem"
+          >
+            No ENS Subscription Active
+          </div>
+        </div>
+        <div v-else>
+          <div
+            class="text-overline text-primary"
+            style="font-size: 0.7rem !important; line-height: 0.8rem"
+          >
+            ENS Subscription Active
+          </div>
+        </div>
+      </div>
+    </v-card-header>
+    <v-card-text v-if="subscriptionActive"> </v-card-text>
+  </v-card>
+</template>
+
+<script>
+//import openapiService from "../services/openapiService";
+
+export default {
+  name: "ActivityLogModule",
+  props: ["devMode"],
+
+  data: () => ({
+    subscriptionActive: false,
+  }),
+  beforeUpdate() {},
+  watch: {
+    quantity: function (value) {
+      console.log(value);
+      if (value) {
+        this.BuySellDisabled = false;
+      } else {
+        this.BuySellDisabled = true;
+      }
+    },
+  },
+};
+</script>
---
+++ src/components/DevModeModule.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-icon
+    @click="showDevTooltip()"
+    v-if="devMode"
+    style="float: right"
+    class="ma-1"
+    size="small"
+    color="secondary"
+    >mdi-information-outline</v-icon
+  >
+</template>
+
+<script>
+export default {
+  name: "DevModeModule",
+  props: ["devMode"],
+  methods: {
+    showDevTooltip() {
+      console.log("show me");
+    },
+  },
+};
+</script>
+
+<style></style>
